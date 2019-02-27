ab669f3d8791bbb63f23448708cf3fa2b601202e
LOGESHWARIM <47376868+LOGESHWARIM@users.noreply.github.com>
2019-02-27 00:29:17 -0800

    Create ex.java

---
+++ ex.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		 for(int i=0;i<n;i++)
+        {
+           System.out.println("Hello");
+        }
+       
+    }
+}
