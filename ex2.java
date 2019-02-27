f803a76c40aca7dbfaf8cc4f3fbd084fc18d513b
LOGESHWARIM <47376868+LOGESHWARIM@users.noreply.github.com>
2019-02-27 00:39:49 -0800

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		 for(int i=1;i<=n;i++)
+        {
+           sum=sum+i;
+        }
+       System.out.println(sum);
+    }
+}
